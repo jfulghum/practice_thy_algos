865bb3d005104d81d7226cb7002b5ed61181b624
Johanna Fulghum <johanna.fulghum@gmail.com>
2019-08-12 10:23:25 -0400

    edge cases

--- longestPrefix.py
+++ longestPrefix.py
@@ -1,15 +1,15 @@
-input = ["flower","flow","flight"]
-
+input = ["flower","flow","flight"] # "fl"
+input2 = ["c","acc","ccc"] # ""
 
 def longestPrefix(input):
+  if len(input) == 0: return ""
   longest_pre = input[0]
   for i in range(1, len(input)):
-    while input[i].find(longest_pre) == -1:
+    while input[i].startswith(longest_pre) == False:
       longest_pre = longest_pre[:-1]
-      if len(longest_pre) == 0: return 0
+      if len(longest_pre) == 0: return ""
   return longest_pre
 
 
-
-
 print(longestPrefix(input))
+print(longestPrefix(input2))
