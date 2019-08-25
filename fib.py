ee4d2680c18641cd1fb9fb7a6715d81e4c4c23ff
Johanna Fulghum <johanna.fulghum@gmail.com>
2019-08-25 09:11:56 -0400

    more fib options

--- fib.py
+++ fib.py
@@ -11,3 +11,16 @@ def fib(n):
 print(fib(6))
 # 1,1,2,3,5,8
 
+
+def fib_recursive(n):
+  if n <= 2: return 1
+  return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+print(fib_recursive(4))
+
+def fib_cache(n, cache = {1:1, 2:1}):
+  if n in cache: return cache[n]
+  cache[n] = fib_cache((n - 1),cache) + fib_cache((n-2),cache)
+  return cache[n]
+
+print(fib_cache(7))
