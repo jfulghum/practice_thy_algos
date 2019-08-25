9fe055dc4545d453516eff877b2ea5955dd8c3ff
Johanna Fulghum <johanna.fulghum@gmail.com>
2019-08-24 20:04:08 -0400

    i love quicksort

---
+++ sorting/quicksort.py
@@ -0,0 +1,6 @@
+def quicksort(array):
+  if len(array) <= 1: return array
+  return quicksort([x for x in array[1:] if x < array[0]]) + [array[0]] + quicksort([x for x in array[1:] if x >= array[0] ])
+
+
+print(quicksort([3,2,4,6,5,4,3]))
