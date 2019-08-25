506dbf17e7661950f1df891d99fbdfe4cfbeb30e
Johanna Fulghum <johanna.fulghum@gmail.com>
2019-08-25 09:29:40 -0400

    pointer bsearch

--- bsearch.py
+++ bsearch.py
@@ -12,3 +12,20 @@ def bsearch(array, target, l, r):
 
 array = [1,2,3,4,5,6,7,8,9]
 print(bsearch(array,8, 0, len(array) - 1))
+
+
+def binarysearch(array, target):
+  left = 0
+  right = len(array) - 1
+  while left <= right:
+    mid = (left + right )//2
+    if array[mid] == target:
+      return mid
+    elif array[mid] < target:
+      left = mid + 1
+    else:
+      right = mid - 1
+  return - 1
+
+
+print(binarysearch(array,8))
