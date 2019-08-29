7411f79cbcf7d7a9e561f4579b4a2b5f2118b358
Johanna Fulghum <johanna.fulghum@gmail.com>
2019-08-29 10:12:32 -0400

    longestSubstringWithoutRepeatingChars

---
+++ longestSubstringWithoutRepeatingChars.py
@@ -0,0 +1,14 @@
+def lengthOfLongestSubstringWithoutRepeatingChars(string):
+  seen = {}
+  start = 0
+  max_length = 0
+  for end, el in enumerate(string):
+    if el in seen:
+      start = max(start, seen[el] + 1)
+    seen[el] = end
+    max_length = max(max_length, end - start + 1)
+  return max_length
+
+print(lengthOfLongestSubstringWithoutRepeatingChars("abcabcbb") == 3)
+#sliding window
+#https://www.youtube.com/watch?v=sZosU8JjVaA
