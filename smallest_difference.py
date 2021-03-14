b3ed36bcfb731bf24c0ebcd46dc7dd9d0d711f29
Johanna Fulghum <johanna.fulghum@gmail.com>
2021-03-13 18:56:27 -0700

    Update smallest_difference.py

--- smallest_difference.py
+++ smallest_difference.py
@@ -1,26 +1,22 @@
-def smallestDifference(arrayOne, arrayTwo):
-    arrayOne.sort()
-	arrayTwo.sort()
+def smallestDifference(x,y):
+	x.sort()
+	y.sort()
+	i1 = 0
+	i2 = 0 
 	current = float("inf")
 	smallest = float("inf")
-	i1 = 0 
-	i2 = 0
-	while i1 < len(arrayOne) and i2 < len(arrayTwo):
-		num1 = arrayOne[i1]
-		num2 = arrayTwo[i2]
+	while i1 < len(x) and i2 < len(y):
+		num1 = x[i1]
+		num2 = y[i2]
 		if num1 == num2:
-			return [num1, num2]
-		elif num1 < num2:
-			current = num2 - num1
+	 		 return [num1, num2]
+		elif num1 < num2: 
 			i1 += 1
+			current = num2 - num1
 		else:
+			i2 +=1
 			current = num1 - num2
-			i2 +=1 
-		
 		if current < smallest:
-			smallest = current
-			smallest_difference = [num1, num2]
-	return smallest_difference
-
-
-				
+	  		smallest = current
+	  		smallestPair = [num1, num2]
+  	return smallestPair 
