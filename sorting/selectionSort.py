45f37d1c93e5f7ea15cc97c4f1bf42a95e4d6158
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-04-16 11:16:49 -0600

    Update selectionSort.py

--- sorting/selectionSort.py
+++ sorting/selectionSort.py
@@ -17,3 +17,23 @@ def swap(array, i, j):
 print(selectionSort(array))
 
 #It minimizies number of swaps, choose when swapping is expensive.
+
+
+# Here is another way. Important thing to remember is for current Index, you must stop 1 before the end. 
+#Eg. for currentIdx in range(0, len(array) -1 ):  or while currentIdx < len(array) - 1:
+  # Otherwise if you go to the VERY end, you're going to be switching the last one out of order. 
+  
+def selectionSort(array: list[int]) -> list[int]:
+    smallestIdx = 0
+    for currentIdx in range(0, len(array) -1):
+        for i in range(currentIdx, len(array)):
+            if array[i] < array[smallestIdx]:
+                smallestIdx = i 
+        array[currentIdx], array[smallestIdx] = array[smallestIdx], array[currentIdx]
+    return array
+
+# Test Cases
+print(selectionSort([]) == [])
+print(selectionSort([1]) == [1])
+print(selectionSort([3, 1, 2, 4])  == [1, 2, 3, 4])
+print(selectionSort([-10, 1, 3, 8, -13, 32, 9, 5]) == [-13, -10, 1, 3, 5, 8, 9, 32])
