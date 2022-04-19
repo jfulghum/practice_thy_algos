843dec9bdf0c8ce14a0945cdea62dc198bc75cd7
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-04-18 20:16:37 -0600

    Update bubbleSort.py

--- sorting/bubbleSort.py
+++ sorting/bubbleSort.py
@@ -8,3 +8,18 @@ def bubbleSort(array):
 
 
 print(bubbleSort([2,3,4,3,2,1,2,3,6]))
+
+
+
+#here's another (i think simpler) way:
+
+def bubbleSort(array):
+  i = 0 
+  while i < len(array):
+    for j in range(i + 1, len(array)):
+      if array[i] > array[j]:
+        array[i], array[j] = array[j], array[i]
+    i += 1 
+  return array
+
+print(bubbleSort([34,3,4,2,4,2,3,4]))
