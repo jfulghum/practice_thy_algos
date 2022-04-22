7746d3ccd630dc959d93670a8e2b4b130ae8a938
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-04-22 15:48:49 -0600

    Update can_match_fellows.py

--- dictionaries_sets/can_match_fellows.py
+++ dictionaries_sets/can_match_fellows.py
@@ -1,1 +1,38 @@
+'''
+Prompt:
+Given an input dictionary mapping Fellows (as string names) to skill ratings, return true if you're able to pair up Fellows with matching skill ratings with no Fellows leftover.
+
+Function Signature:
+function canMatchFellows(input)
+
+Examples
+{"oliver": 3, "pixel": 3, "pinky": 5, "tobey": 5} => true
+{"oliver": 3, "pixel": 4, "pinky": 5, "tobey": 5} => false
+{} => false
+
+
+{"Jorie":1, "Johanna":2, "Juliette":3, "Kendallene":1, "David":2, "Keith";3} => true
+'''
+
+#Identify Simple Solution (Brute Force is Okay)
+# set()
+
+# {"oliver": 3, "pixel": 3, "pinky": 5, "tobey": 5}
+# ^ 
+# loop through and add value to set if there, remove if not, at the end return if len of set
+
+
+def canMatchFellows(ratingbyFellow):
+    result = set()
+    for _, value in ratingbyFellow.items():
+        if value in result:
+            result.remove(value)
+        else:
+            result.add(value)
+    return not len(result) 
+
+
+print(canMatchFellows({"oliver": 3, "pixel": 3, "pinky": 5, "tobey": 5})) # true
+print(canMatchFellows({"oliver": 3, "pixel": 3, "pinky": 4, "tobey": 5})) # false
+print(canMatchFellows({"oliver": 3, "pixel": 3, "pinky": 5, "tobey": 5, 'Johanna': 4})) # false
 
