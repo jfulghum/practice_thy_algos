6656dc57e06e638656998bd9fe848f2908249861
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-04-29 09:03:00 -0600

    Update maxElementLinkedList.py

--- LinkedList/maxElementLinkedList.py
+++ LinkedList/maxElementLinkedList.py
@@ -1,14 +1,22 @@
 # find Max Element in a Linked List (recursive)"
 
-class LinkedList:
-    def __init__(self, value):
+class ListNode:
+    def __init__(self, value = 0, next = None): 
         self.value = value
-        self.next = None
+        self.next = next
+        
+def findMax(node: ListNode, max=0) -> int:
+    if node is None:
+      return max
+    if node.value > max:
+      max = node.value
+    return findMax(node.next, max)
 
-
-def findMax(node, max=0):
-  if node is None:
-    return max
-  if node.value > max:
-    max = node.value
-  return findMax(node.next)
+# Test Cases
+LL1 = ListNode(1, ListNode(4, ListNode(5, ListNode(1))))
+LL2 = ListNode(7, ListNode(1, ListNode(5, ListNode(1))))
+LL3 = ListNode(-1, ListNode(-3, ListNode(-5, ListNode(0, ListNode(-11)))))
+print(findMax(LL1)) # 5
+print(findMax(LL2)) # 7
+print(findMax(LL3)) # 0
+print(findMax(ListNode(1))) # 1
