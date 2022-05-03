6b9580d4c2fc29fa5ccece1cc7c59c96b8df6d36
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-05-03 10:55:01 -0600

    Create delete_duplicate_nodes_in_sorted_linkedlist.py

---
+++ LinkedList/delete_duplicate_nodes_in_sorted_linkedlist.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        while curr:
+            nextNode = curr.next
+            while nextNode and curr.val == nextNode.val:
+                nextNode = nextNode.next
+            curr.next = nextNode
+            curr = curr.next
+        return head
