a1bc076d185ed303ef98d2127256a36c3c03bc18
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-05-04 21:28:07 -0600

    Update breadthFirstSearch.py

--- breadthFirstSearch.py
+++ breadthFirstSearch.py
@@ -54,3 +54,56 @@ dogs = {
 
 print(findHat(dogs, 'Loki')) # returns 'Ivy'
 
+// Oliver is missing his favorite hat and is asking his friends at the dog park if they've seen it. Each dog either has seen it or suggests a list of other dogs to ask. Return the name of the dog who has seen the hat.
+
+/**
+ * Approaches:
+ * queue = []
+ * Look up bestfriend (Loki -> list)
+ * push the keys into the queue 
+ * while the queue is filled
+ *  dogFromQueue = queue.pop()
+ *  check if the "HAT" exists from the dogFromQueue
+ *  else the dogFromQueue
+ *    check the dog has other members and push them into the queue
+ * 
+ * return null
+ */
+
+function findHat(dogs, bestFriend){
+  let queue = [...dogs[bestFriend]]; 
+  let dogsAsked = new Set()
+  while(queue.length) {
+    let dogFromQueue = queue.pop();
+    if (!dogsAsked.has(dogFromQueue)) {
+      if(dogs[dogFromQueue][0] == "HAT") {
+        return dogFromQueue;
+      } else {
+        dogsAsked.add(dogFromQueue)
+        // push the additonal dogs into the queue
+        dogs[dogFromQueue].map(dog =>  {
+          if (!dogsAsked.has(dog)) {
+            queue.push(dog)
+          } 
+        });            
+      }
+    } 
+  }
+  return null;
+}
+
+// Target runtime and space complexity:
+//  O(n)
+// Examples:
+const dogs = {
+  'Carter': ['Fido', 'Ivy'],
+  'Ivy': ["HAT"], // Ivy has seen the hat
+  'Loki': ['Snoopy'],
+  'Pepper': ['Snoopy', 'Ivy'], 
+  'Snoopy': ['Pepper'],
+  'Fido': []
+};
+console.log(findHat(dogs, 'Loki')); // returns 'Ivy'
+console.log(findHat(dogs, 'Fido')) // null
+console.log(findHat(dogs, 'Pepper')); // returns 'Ivy'
+
