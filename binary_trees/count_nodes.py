66d68f19d13f68c1a9067a5373f1c3c51f8824b6
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-05-22 09:50:34 -0600

    Create count_nodes.py

---
+++ binary_trees/count_nodes.py
@@ -0,0 +1,48 @@
+class BTNode:
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def arrayifyTree(root):
+    if root is None:
+        return []
+    queue = []
+    array = []
+    queue.append(root)
+    while (len(queue) != 0):
+        node = queue.pop(0)
+        array.append(node.value)
+        if (node.left):
+            queue.append(node.left)
+        if (node.right):
+            queue.append(node.right)
+    return array
+
+
+class TreeNode:
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+        
+def count_tree(root: TreeNode) -> int:
+  if root is None:
+    return 0
+  count = 0
+  stack = [root]
+  while stack:
+    curr = stack.pop()
+    count += 1
+    if curr.right:
+      stack.append(curr.right)
+    if curr.left:
+      stack.append(curr.left)
+  return count
+
+
+# Test Cases
+print(count_tree(None), 0)
+print(count_tree(TreeNode(1, TreeNode(2), TreeNode(3))), 3)
+print(count_tree(TreeNode(2, TreeNode(29, TreeNode(26)), TreeNode(4, None, TreeNode(2, TreeNode(9))))), 6)
+print(count_tree(TreeNode()), 1)
