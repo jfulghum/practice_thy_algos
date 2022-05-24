fc02740e2f0e8f40a3985cc0838817f40763217a
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-05-24 10:00:48 -0600

    Update findOnlyChildren.js

--- binary_trees/findOnlyChildren.js
+++ binary_trees/findOnlyChildren.js
@@ -1,3 +1,6 @@
+// this works in JS but not in Python b/c every time you call this function in Python, 
+// b/c you have the array as the default [] its a mutable object and it will keep the old stuff from the old function call and mess everything up
+
 function findOnlyChildren(root, result = []) {
   if (!root) return;
 
