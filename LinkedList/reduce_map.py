21c42d28271e1f2388a457d874802716dfeee064
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-05-27 19:37:14 -0600

    Update and rename reduce.py to reduce_map.py

--- LinkedList/reduce_map.py
+++ LinkedList/reduce_map.py
@@ -11,3 +11,18 @@ def reduce(head, accumulator_func, initialVal):
 reduce([1, 5, 10], function(acc, curr) {
   return acc + curr
 }, 0)
+
+
+
+def map(head, mapper_func):
+  if head is None:
+    return None
+  new_node = ListNode(0)
+  new_curr = new_node
+  curr = head
+  while curr:
+    new_curr.next = mapper_func(curr)
+    new_curr = new_curr.next
+    curr = curr.next
+  return new_node.next
+    
