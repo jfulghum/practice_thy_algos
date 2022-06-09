1d6366327534552635a86b718e12fcd98fa2cb4b
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-09 16:42:27 -0600

    Update serialize_string_to_binary_trees.py

--- serialize/serialize_string_to_binary_trees.py
+++ serialize/serialize_string_to_binary_trees.py
@@ -1,3 +1,9 @@
+class TreeNode:
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
 class Solution:
     def str2tree(self, s: str) -> TreeNode:
         
