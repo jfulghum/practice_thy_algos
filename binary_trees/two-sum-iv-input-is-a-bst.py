7def18d3421ae10cb9a3744160f0da1dad834ca7
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-12 14:04:35 -0600

    Update two-sum-iv-input-is-a-bst.py

--- binary_trees/two-sum-iv-input-is-a-bst.py
+++ binary_trees/two-sum-iv-input-is-a-bst.py
@@ -15,3 +15,29 @@ class Solution:
                 l.add(node.val)
             return dfs(node.left) or dfs(node.right)
         return dfs(root)
+
+ 
+#this is better b/c it uses that the Tree is sorted.
+class Solution:        
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        def inorder(root, result):
+            if (root is None):
+                return False
+            inorder(root.left, result)
+            result.append(root.val)
+            inorder(root.right, result)
+
+        result = []
+        inorder(root, result)
+        l = 0
+        r = len(result) - 1
+        while (l < r):
+            sum = result[l] + result[r]
+            if (sum == k):
+                return True;
+            if (sum < k):
+                l += 1
+            else:
+                r -= 1
+        return False
+    
