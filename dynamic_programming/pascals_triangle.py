68a9e9cd4492d86f201cb804ec75ea5a702ab797
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-18 13:38:04 -0600

    Create pascals_triangle.py

---
+++ dynamic_programming/pascals_triangle.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/pascals-triangle/
+
+class Solution:
+    def generate(self, num_rows: int) -> List[List[int]]:
+        triangle = []
+
+        for row_num in range(num_rows):
+            # The first and last row elements are always 1.
+            row = [None for _ in range(row_num + 1)]
+            row[0], row[-1] = 1, 1
+
+            # Each triangle element is equal to the sum of the elements
+            # above-and-to-the-left and above-and-to-the-right.
+            for j in range(1, len(row) - 1):
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+            triangle.append(row)
+
+        return triangle
+        
+        
+# Input: numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
