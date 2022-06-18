9d4e356832e267bb44ddc1fd504e94bd7a6a25ed
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-18 15:01:17 -0600

    Create triangle.py

---
+++ dynamic_programming/triangle.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/triangle/
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for row in range(1, len(triangle)):
+            for col in range(row + 1):
+                smallest_above = math.inf
+                if col > 0:
+                    smallest_above = triangle[row - 1][col - 1]
+                if col < row:
+                    smallest_above = min(smallest_above, triangle[row - 1][col])
+                triangle[row][col] += smallest_above
+        return min(triangle[-1])
+      
+# Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+# Output: 11
+# Explanation: The triangle looks like:
+#    2
+#   3 4
+#  6 5 7
+# 4 1 8 3
+# The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
