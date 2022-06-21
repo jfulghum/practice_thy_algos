2867172ceb0f302aa71b837187051879f03b2190
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-20 22:50:28 -0600

    Update pascals_triangle.py

--- dynamic_programming/pascals_triangle.py
+++ dynamic_programming/pascals_triangle.py
@@ -1,4 +1,9 @@
 # https://leetcode.com/problems/pascals-triangle/
+# Given an integer numRows, return the first numRows of Pascal's triangle.
+
+# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
 
 class Solution:
     def generate(self, num_rows: int) -> List[List[int]]:
