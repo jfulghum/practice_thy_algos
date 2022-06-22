9070758bf8fd01e6853e9eb18f3f0070f891c7aa
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-21 21:20:35 -0600

    Update in_order.py

--- binary_trees/in_order.py
+++ binary_trees/in_order.py
@@ -5,8 +5,32 @@ def inOrderTraversal(root):
         if node:
             if visited:
                 res.append(node.value)
-            else:  # inorder: left -> root -> right
+            else:  
                 stack.append((node.right, False))
                 stack.append((node, True))
                 stack.append((node.left, False))
     return res
+
+
+
+#recursive
+def inOrderTraverse(tree, array):
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
+
+def preOrderTraverse(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
+    return array
+
+def postOrderTraverse(tree, array):
+    if tree is not None:
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
