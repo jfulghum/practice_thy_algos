2261ad7a7eeecfc85b77122ae9f0c83a5356d7bf
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-06-24 20:01:04 -0600

    Create matrix_bottom_up.js

---
+++ vinit/matrix_bottom_up.js
@@ -0,0 +1,7 @@
+
+Given N and M representing a matrix of size N by M, how many unique paths are there to get from the top left cell to the top right cell of the matrix. The only valid moves are:
+- 1 step to the right
+- 1 step up and to the right
+- 1 step down and to the right
+
+Followup: how many unique paths pass through i,j
