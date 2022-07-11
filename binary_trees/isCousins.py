e93f6555dd14dffeb34ee9496e8df6524da50eb1
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-07-11 13:34:00 -0600

    Create isCousins.py

---
+++ binary_trees/isCousins.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        queue = collections.deque([root])
+        
+        while queue: 
+            
+            cousins = False 
+            siblings = False
+            
+            length = len(queue)
+            
+            for _ in range(length):
+                
+                node = queue.popleft()
+                
+                if node is None:
+                    siblings = False
+                else:
+                    if node.val == x or node.val == y:
+                        
+                        if not cousins:
+                            siblings = True
+                            cousins = True
+                        else:
+                            return not siblings
+                        
+                    queue.append(node.left) if node.left else None
+                    queue.append(node.right) if node.right else None
+                    
+                    queue.append(None)
+                
+            if cousins:
+                return False
