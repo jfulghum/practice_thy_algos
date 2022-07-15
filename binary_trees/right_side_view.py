989831be7da300a9109d599a55455f85f79f12ab
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-07-14 20:54:02 -0600

    Update right_side_view.py

--- binary_trees/right_side_view.py
+++ binary_trees/right_side_view.py
@@ -21,3 +21,27 @@ def right_side_view(root):
         result.append(node.value)
         
     return result
+
+
+
+
+# very similar:
+
+import collections 
+def solution(t):
+    if t is None:
+        return []
+    nextLevel = collections.deque([t])
+    result = []
+    while nextLevel:
+        currentLevel = nextLevel
+        nextLevel = collections.deque()
+        
+        for node in currentLevel:
+            if node.left:
+                nextLevel.append(node.left)
+            if node.right:
+                nextLevel.append(node.right)
+        result.append(currentLevel[-1].value)
+        
+    return result
