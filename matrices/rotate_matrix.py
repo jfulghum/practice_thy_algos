b229a55e1cbab1ee11454ca7acd906e03efd58e5
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-07-15 12:33:23 -0600

    Create rotate_matrix.py

---
+++ matrices/rotate_matrix.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/rotate-image/submissions/
+
+# Transposing a matrix and then relecting it ( A reflection is the flipping of a point or figure over a line of reflection (the mirror line))
+# results in a matrix.
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+        
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j] 
