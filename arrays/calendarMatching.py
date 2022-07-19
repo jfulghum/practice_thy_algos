7af8ea5a76ec7b70f5451f306c2e4bb483735773
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-07-18 21:06:21 -0600

    Update calendarMatching.py

--- arrays/calendarMatching.py
+++ arrays/calendarMatching.py
@@ -69,3 +69,25 @@ def minutesToTime(minutes):
     hoursString = str(hours)
     minutesString = "0" + str(mins) if mins < 10 else str(mins)
     return hoursString + ":" + minutesString
+
+
+
+calendar1 = [
+    ["9:00", "10:30"],
+    ["12:00", "13:00"],
+    ["16:00", "18:00"]]
+calendar2 = [
+    ["10:00", "11:30"],
+    ["12:30", "14:30"],
+    ["14:30", "15:00"],
+    ["16:00", "17:00"]]
+dailyBounds1 = ["9:00", "20:00"]
+dailyBounds2  = ["10:00", "18:30"]
+meetingDuration = 30
+
+
+
+
+print(calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration))
+
+# [['11:30', '12:00'], ['15:00', '16:00'], ['18:00', '18:30']]
