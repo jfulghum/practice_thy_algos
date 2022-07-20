17e19028e62e163612ce425cdc1f53b958fcad2e
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-07-19 19:27:43 -0600

    Create group_ones_min_swap.js

---
+++ arrays/group_ones_min_swap.js
@@ -0,0 +1,49 @@
+// Group 0s and 1s
+
+// Given an array of 0s and 1s, what is the minimum number of moves needed to group all 0s on one side and 1s on the other side. A "move" is a swap between any adjacent positions.
+
+// Examples
+
+// [0, 1] => 0, no swaps are needed since they are already grouped.
+// [0, 1, 0] => 1, swap 1 with either 0 to group them.
+// [1, 0, 1, 1, 0] => 2, swap 0 with 1 then swap it again with the next 1.
+
+// [0,1,0,0,0] => 1
+
+// [1, 1, 1, 1, 1, 0, 0, 1, 0]  
+//.                      ^ 
+//                 ^
+
+// for loop 
+//.   checks if element is 1, if yes
+//      swapCount += pointer difference
+//      increment slow pointer
+//.     
+// similarly, going from the other direction. 
+// for loop first checks if it is 0 [..]
+
+// return min of both swap counts.
+
+
+// Function Signature
+
+function minSwaps(array){
+  let swapCount1 = 0
+  let swapCount2 = 0
+  let slowPointer = 0
+  
+  // loop
+  for(let i=0; i<array.length; i++){
+      if(array[i]==1){
+        swapCount1 += i - slowPointer;
+
+        slowPointer++;
+      }
+
+
+  }
+
+}
+
+
+
