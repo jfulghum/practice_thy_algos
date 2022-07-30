914e8255e0cea83fc54aeae2cb6d245b1e4222b0
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-07-30 11:57:11 -0600

    Create count_islands.py

---
+++ recursion/count_islands.py
@@ -0,0 +1,23 @@
+def solution(arr):
+    total = 0
+    for y in range(len(arr)):
+        for x in range(len(arr[y])):
+            if arr[y][x] == 1:
+                total += 1
+                dfs(x,y,arr)
+    return total
+
+def dfs(x, y, arr):
+    if y < 0 or y >= len(arr) or x < 0 or x >= len(arr[y]):
+        return
+    if arr[y][x] == 0:
+        return
+    arr[y][x] = 0
+    dfs(x+1, y, arr)
+    dfs(x-1, y, arr)
+    dfs(x, y-1, arr)
+    dfs(x, y+1, arr)
+
+print(solution([[1,1,1],
+                [1,0,0],
+                [0,1,0]])
