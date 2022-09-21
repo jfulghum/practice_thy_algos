8a9287fe06c47222c6bb0646b4cebdc9b027d12a
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-09-21 14:12:07 -0600

    Update subsets.py

--- recursion/subsets.py
+++ recursion/subsets.py
@@ -14,3 +14,40 @@ class Solution:
         return result
         
     
+
+    
+    
+# https://neetcode.io/practice
+
+#     another way!
+
+# input: [1,2,3]
+# [1,2]
+# res: [[1],[2],[3],[1,2],[2,3],[1,3],[1,2,3],[]]
+array = [1,2,3]
+
+#arrays are passed by reference, so anytime we change the array, it will mess up all the future
+
+def subsets(array):
+  result = []
+  
+  def helper(index, part):
+    partCpy = part.copy()
+    
+    if index >= len(array):
+      result.append(partCpy)
+      return 
+      
+    # choose
+    partCpy.append(array[index])
+    helper(index + 1, partCpy)
+  
+    # don't choose
+    partCpy.pop()
+    helper(index + 1, partCpy)
+  
+  helper(0, [])
+  return result
+  
+
+print(subsets(array))
