b4861e2f37a1c520b2181005e3021bb6575e3e01
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-10-03 15:46:44 -0600

    Update is_palindrome_linked_list.py.py

--- LinkedList/is_palindrome_linked_list.py.py
+++ LinkedList/is_palindrome_linked_list.py.py
@@ -27,3 +27,37 @@ def solution(head):
     return ispalin
     
 
+# another way:
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def reverseLL(head):
+    prevNode, currNode = None, head
+    while currNode:
+        nextNode = currNode.next
+        currNode.next = prevNode
+        prevNode, currNode = currNode, nextNode
+    return prevNode
+    
+def compare(list1, list2):
+    while list1 and list2:
+        if list1.value == list2.value:
+            list1 = list1.next
+            list2 = list2.next
+        else:
+            return False
+    return True
+
+def solution(head):
+    reverse = reverseLL(head)
+    
+    return compare(head, reverse)
+    
+#  1 -> 2 -> 3
+#  3 -> 2 -> 1   
+
+        
