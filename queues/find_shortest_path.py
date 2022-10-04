27a639f5ccd6d43f14aebdce862ee28cde8004a5
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-10-04 17:57:07 -0600

    Update find_shortest_path.py

--- queues/find_shortest_path.py
+++ queues/find_shortest_path.py
@@ -22,3 +22,45 @@ def find_shortest_path(flights, start, end):
     for cityName in flights[city]:
       if cityName not in visited:
         queue.append(cityName, )
+
+        
+        
+        
+
+end: 'New York'
+
+sp('Seattle')       // shortestPath: 1
+  sp('Dallas')      // shortestPath: Infinity
+    sp('Detroit')  => null
+
+*/
+
+
+const shortestFlightPath = (start, end) => {
+  if (start === end) {
+    return 0;
+  }
+  if (!flights[start]) {
+    return null;
+  }
+
+  let shortestPath = Infinity;
+  flights[start].forEach((possibleDest) => {
+    const shortestPathFromDest = shortestFlightPath(possibleDest, end);
+    if (shortestPathFromDest !== null) {
+      shortestPath = Math.min(shortestPath, shortestPathFromDest)
+    }
+  });
+
+  return shortestPath === Infinity ? null : shortestPath + 1;
+}
+
+console.log(shortestFlightPath('Seattle', 'New York'))
+
+
+const flights = {
+  'Seattle': ['Boston', 'Austin', 'Portland', 'Dallas'],
+  'Dallas': ['Detroit'],
+  'Austin': ['Raleigh', 'DC'],
+  'Boston' : ['Milwaukee']
+}
