5de2c05d2a3c22ac38805472110cfdd6d7b7edb1
Johanna Fulghum <johanna.fulghum@gmail.com>
2022-11-15 14:09:08 -0700

    Update insert_LL.py

--- LinkedList/insert_LL.py
+++ LinkedList/insert_LL.py
@@ -1,3 +1,5 @@
+#insert in order 
+
 def solution(list, target):
     if list is None:
         return ListNode(target)
@@ -18,6 +20,27 @@ def solution(list, target):
         
     curr.next = ListNode(target)
     return list
+
+
+
+# insert before target
+
+def solution(head, target, value):
+    curr = head
+    if curr.value == target:
+        node = ListNode(value)
+        node.next = curr 
+        return node
+    
+    while curr.next:
+        prev = curr
+        curr = curr.next
+        if curr and curr.value == target:
+            prev.next = ListNode(value)
+            prev.next.next = curr
+            return head
+            
+    return head
             
         
             
