d327e4e52e16a504f2835f905c48dcef22fdddd2
Johanna Fulghum <johanna.fulghum@gmail.com>
2023-01-06 14:25:05 -0300

    Create DFS.py

---
+++ binary_trees/DFS.py
@@ -0,0 +1,30 @@
+# ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+# Iterative Solution
+# ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+def find_dfs(node: TreeNode, target: int) -> int:
+    if not node:
+        return False
+
+    stack = [node]    
+    while stack:
+        curr = stack.pop()
+        if curr.value == target:
+            return True
+        if curr.left:
+            stack.append(curr.left)
+        if curr.right:
+            stack.append(curr.right)
+        
+    return False
+
+# ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+# ⬛️ Recursive Solution
+# ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+def find_dfs_rec(node, target):
+    if not node:
+        return False
+
+    if node.value == target:
+        return True
+
+    return find_dfs_rec(node.left, target) or find_dfs_rec(node.right, target)
