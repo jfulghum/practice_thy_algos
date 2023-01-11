22bb309a4a65cb5c55beb443a87ff0cf2e7b5c1f
Johanna Fulghum <johanna.fulghum@gmail.com>
2023-01-11 08:54:39 -0300

    Update DFS.py

--- binary_trees/DFS.py
+++ binary_trees/DFS.py
@@ -1,3 +1,21 @@
+class TreeNode:
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def find_dfs(root: TreeNode, target: int) -> bool:
+    # Write your code here. 
+    pass
+
+# Test Cases
+tree1 = TreeNode(3, TreeNode(29, TreeNode(2)), TreeNode(4, None, TreeNode(2, TreeNode(9))))
+print(find_dfs(None, 1), False)
+print(find_dfs(tree1, 9), True)
+print(find_dfs(tree1, 1), False)
+print(find_dfs(tree1, 2), True)
+print(find_dfs(TreeNode(1), 2), False)
+
 # ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
 # Iterative Solution
 # ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
