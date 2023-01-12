ab0171408b1897b4c05ddeecf2a08354c1ad8ba1
Johanna Fulghum <johanna.fulghum@gmail.com>
2023-01-12 17:35:49 -0300

    Update dfs_bfs.py

--- graph/dfs_bfs.py
+++ graph/dfs_bfs.py
@@ -51,3 +51,17 @@ traverse_bfs_levels(graph, 'A')
 #      A
 #    B        C
 # D    E         F
+
+
+# this is a VERY useful pattern - just by going through it by iterating, it's like you ARE popping left, you are going FIFO
+
+def traverse_bfs_levels2(graph, current_node):
+    queue = deque([current_node])
+    while queue:
+        next_q = []
+        for node in queue:
+            print(node, end=' ')
+            for child in graph[node]:
+                next_q.append(child)
+        print()
+        queue = next_q
