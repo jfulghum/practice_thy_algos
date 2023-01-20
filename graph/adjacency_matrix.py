fbc89c0d081f791efd2580b2b2ddd821e38914c6
Johanna Fulghum <johanna.fulghum@gmail.com>
2023-01-20 14:35:00 -0300

    Update adjacency_matrix.py

--- graph/adjacency_matrix.py
+++ graph/adjacency_matrix.py
@@ -20,3 +20,18 @@ def solution(matrix, v1, v2):
     for child in adj_list[current]:
       q.append((child, depth +1))
   return -1
+
+
+
+matrix = 
+[[false,false,true], 
+ [false,false,true], 
+ [true,true,false]]
+vertex1 = 0
+vertex2 = 1
+
+solution(matrix, vertex1, vertex2)
+# Output:
+# 2
+# Expected Output:
+# 2
